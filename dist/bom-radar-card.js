adf0798af90d8eae7d596365f6faca5b97fbec81
Michael Kenny <git@mmkenny.net>
2024-10-24 02:00:54 +1030

    Removed 120px width from timestamp. Again.

--- dist/bom-radar-card.js
+++ dist/bom-radar-card.js
@@ -13475,7 +13475,7 @@ let BomRadarCard = class BomRadarCard extends s$1 {
                       <div id="progress-bar" style="height:8px;width:0; background-color: #ccf2ff;"></div>
                     </div>
                     <div id="bottom-container" class="light-links" style="height: 32px; background-color: white;">
-                      <div id="timestampid" class="text-container" style="width: 120px; height: 32px; float:left; position: absolute;">
+                      <div id="timestampid" class="text-container" style="height: 32px; float:left; position: absolute;">
                         <p id="timestamp"></p>
                       </div>
                       <div id="attribution" class="text-container-small" style="height: 32px; float:right;">
